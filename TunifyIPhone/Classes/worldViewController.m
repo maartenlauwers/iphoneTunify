5d06c061014a424a8cbbbf42719217a39946e852
Maarten Lauwers <maartenlauwers007@telenet.be>
2010-03-17 21:36:42 +0100

    3D arrow works, still a memory leak

--- TunifyIPhone/Classes/worldViewController.m
+++ TunifyIPhone/Classes/worldViewController.m
@@ -12,7 +12,10 @@
 #import "ConstantsAndMacros.h"
 #import "OpenGLCommon.h"
 
+
 @implementation worldViewController
+
+static Vertex3D *normals;
 
 @synthesize strPubName;
 @synthesize glView;
@@ -67,6 +70,54 @@
 }
 
 
+-(Vertex3D *) vertexNormals:(Vertex3D*)vertices nVertices:(int)nVertices faces:(GLubyte*)arrowFaces nFaces:(int)nFaces 
+{
+	
+	
+	Vector3D *surfaceNormals = calloc(nFaces, sizeof(Vector3D));
+	
+	// Calculate the surface normal for each triangle
+	
+	for (int i = 0; i < nFaces; i++)
+	{
+		Vertex3D vertex1 = vertices[arrowFaces[(i*3)]];
+		Vertex3D vertex2 = vertices[arrowFaces[(i*3)+1]];
+		Vertex3D vertex3 = vertices[arrowFaces[(i*3)+2]];
+		Triangle3D triangle = Triangle3DMake(vertex1, vertex2, vertex3);
+		Vector3D surfaceNormal = Triangle3DCalculateSurfaceNormal(triangle);
+		Vector3DNormalize(&surfaceNormal);
+		surfaceNormals[i] = surfaceNormal;
+	}
+	
+	Vertex3D *normals = calloc(nVertices, sizeof(Vertex3D));
+	
+	for (int i = 0; i < nVertices; i++)
+	{
+		int faceCount = 0;
+		for (int j = 0; j < nFaces; j++)
+		{
+			BOOL contains = NO;
+			for (int k = 0; k < 3; k++)
+			{
+				if (arrowFaces[(j * 3) + k] == i)
+					contains = YES;
+			}
+			if (contains)
+			{
+				faceCount++;
+				normals[i] = Vector3DAdd(normals[i], surfaceNormals[j]);
+			}
+		}
+		
+		normals[i].x /= (GLfloat)faceCount;
+		normals[i].y /= (GLfloat)faceCount;
+		normals[i].z /= (GLfloat)faceCount;
+	}
+	return normals;
+}
+
+
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -110,6 +161,88 @@
 	[self.view addSubview:glView];
 	NSLog(@"glView created");
 	
+	
+	static const Vertex3D vertices[]= {
+		
+		// pointer sides
+		{0, 0.0f, -1.0f},					// 0
+		{0, -0.3f, -1.0f},					// 1
+		{-0.5f, 0.0f, -0.2f},				// 2
+		{-0.5f, -0.3f, -0.2f},				// 3
+		{0.5f, 0.0f, -0.2f},				// 4
+		{0.5f, -0.3f, -0.2f},				// 5
+		
+		// pointer roof
+		{0, 0.0f, -1.0f},					// 6
+		{-0.5f, 0.0f, -0.2f},				// 7
+		{0.5f, 0.0f, -0.2f},				// 8
+		
+		// box left side	
+		{-0.15f, 0.0f, -0.2f},				// 9
+		{-0.15f, -0.3f, -0.2f},				// 10
+		{-0.15f, 0.0f, 1.5f},				// 11
+		{-0.15f, -0.3f, 1.5f},				// 12
+		
+		// box right side
+		{0.15f, 0.0f, -0.2f},				// 13
+		{0.15f, -0.3f, -0.2f},				// 14
+		{0.15f, 0.0f, 1.5f},				// 15
+		{0.15f, -0.3f, 1.5f},				// 16
+		
+		// box closest side
+		{0.15f, 0.0f, 1.5f},				// 17
+		{0.15f, -0.3f, 1.5f},				// 18
+		{-0.15f, 0.0f, 1.5f},				// 19
+		{-0.15f, -0.3f, 1.5f},				// 20
+		
+		// box top side
+		{0.15f, 0.0f, -0.2f},				// 21
+		{-0.15f, 0.0f, -0.2f},				// 22
+		{0.15f, 0.0f, 1.5f},				// 23
+		{-0.15f, 0.0f, 1.5f}				// 24
+		
+    };
+    
+    static const GLubyte arrowFaces[] = {
+		// pointer sides
+		0, 2, 1,
+		2, 1, 3,
+		0, 4, 5,
+		0, 5, 1,
+		4, 2, 3,
+		4, 3, 5,
+		
+		// pointer roof
+		6, 7, 8,
+		
+		// box left side
+		9, 11, 12,
+		9, 12, 10,
+		
+		// box right side
+		13, 15, 14,
+		15, 16, 14,
+		
+		// box closest side
+		17, 19, 20,
+		17, 18, 20,
+		
+		// box top side
+		21, 23, 24,
+		21, 22, 24,
+    };
+	
+	
+	
+	
+	
+	if (normals == NULL) {
+		int nVertices = sizeof(vertices) / sizeof(Vertex3D);
+		int nFaces = sizeof(arrowFaces) / (sizeof(GLubyte) * 3);
+		normals = [self vertexNormals:vertices nVertices:nVertices faces:arrowFaces nFaces:nFaces];
+	}
+	 
+	
 	glView.animationInterval = 1.0 / kRenderingFrequency;
 	[glView startAnimation];
 
@@ -138,63 +271,6 @@
 	NSLog(@"Drawing view");
     static GLfloat rot = 0.0;
     
-    // This is the same result as using Vertex3D, just faster to type and
-    // can be made const this way
-	/*
-    static const Vertex3D vertices[]= {
-        {0, -0.525731, 0.850651},             // vertices[0]
-        {0.850651, 0, 0.525731},              // vertices[1]
-        {0.850651, 0, -0.525731},             // vertices[2]
-        {-0.850651, 0, -0.525731},            // vertices[3]
-        {-0.850651, 0, 0.525731},             // vertices[4]
-        {-0.525731, 0.850651, 0},             // vertices[5]
-        {0.525731, 0.850651, 0},              // vertices[6]
-        {0.525731, -0.850651, 0},             // vertices[7]
-        {-0.525731, -0.850651, 0},            // vertices[8]
-        {0, -0.525731, -0.850651},            // vertices[9]
-        {0, 0.525731, -0.850651},             // vertices[10]
-        {0, 0.525731, 0.850651}               // vertices[11]
-    };
-    
-    static const Color3D colors[] = {
-		{1.0, 0.0, 0.0, 1.0},
-		{1.0, 0.5, 0.0, 1.0},
-		{1.0, 1.0, 0.0, 1.0},
-		{0.5, 1.0, 0.0, 1.0},
-		{0.0, 1.0, 0.0, 1.0},
-		{0.0, 1.0, 0.5, 1.0},
-		{0.0, 1.0, 1.0, 1.0},
-		{0.0, 0.5, 1.0, 1.0},
-		{0.0, 0.0, 1.0, 1.0},
-		{0.5, 0.0, 1.0, 1.0},
-		{1.0, 0.0, 1.0, 1.0},
-		{1.0, 0.0, 0.5, 1.0}
-    };
-    
-    static const GLubyte icosahedronFaces[] = {
-        1, 2, 6,
-        1, 7, 2,
-        3, 4, 5,
-        4, 3, 8,
-        6, 5, 11,
-        5, 6, 10,
-        9, 10, 2,
-        10, 9, 3,
-        7, 8, 9,
-        8, 7, 0,
-        11, 0, 1,
-        0, 11, 4,
-        6, 2, 10,
-        1, 6, 11,
-        3, 5, 10,
-        5, 4, 11,
-        2, 7, 9,
-        7, 1, 0,
-        3, 9, 8,
-        4, 8, 0,
-    };
-	 */
-	
 	static const Vertex3D vertices[]= {
 		
 		// pointer sides
@@ -238,45 +314,45 @@
     
     static const Color3D colors[] = {
 		// pointer sides
-		{1.0, 0.0, 0.0, 1.0},
-		{1.0, 0.0, 0.0, 1.0},
-		{0.0, 1.0, 0.0, 1.0},
-		{0.0, 1.0, 0.0, 1.0},
-		{0.0, 0.0, 1.0, 1.0},
-		{0.0, 0.0, 1.0, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
 		
 		// pointer roof
-		{0.5, 0.5, 0.5, 1.0},
-		{0.5, 0.5, 0.5, 1.0},
-		{0.5, 0.5, 0.5, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
 		
 		//box left side
-		{0.5, 0.5, 0.5, 1.0},
-		{0.5, 0.5, 0.5, 1.0},
-		{0.5, 0.5, 0.5, 1.0},
-		{0.5, 0.5, 0.5, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
 		
 		//box right side
-		{0.5, 0.5, 0.1, 1.0},
-		{0.5, 0.5, 0.1, 1.0},
-		{0.5, 0.5, 0.1, 1.0},
-		{0.5, 0.5, 0.1, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
 		
 		//box closest side
-		{0.5, 0.2, 0.5, 1.0},
-		{0.5, 0.2, 0.5, 1.0},
-		{0.5, 0.2, 0.5, 1.0},
-		{0.5, 0.2, 0.5, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
 		
 		//box top side
-		{0.8, 0.5, 0.5, 1.0},
-		{0.8, 0.5, 0.5, 1.0},
-		{0.8, 0.5, 0.5, 1.0},
-		{0.8, 0.5, 0.5, 1.0}
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0},
+		{1.0, 0.6, 0.2, 1.0}
 		 
     };
     
-    static const GLubyte icosahedronFaces[] = {
+    static const GLubyte arrowFaces[] = {
 		// pointer sides
 		0, 2, 1,
 		2, 1, 3,
@@ -286,7 +362,7 @@
 		4, 3, 5,
 		
 		// pointer roof
-		6, 7, 8,
+		6, 8, 7,
 		
 		// box left side
 		9, 11, 12,
@@ -303,13 +379,6 @@
 		// box top side
 		21, 23, 24,
 		21, 22, 24,
-		
-		/*
-		5, 3, 4,
-		6, 4, 2,
-		5, 1, 6,
-		1, 2, 6,
-		 */
     };
     
     glLoadIdentity();
@@ -321,28 +390,36 @@
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, vertices);
     glColorPointer(4, GL_FLOAT, 0, colors);
+	glNormalPointer(GL_FLOAT, 0, normals);
+
 	
-    glDrawElements(GL_TRIANGLES, 45, GL_UNSIGNED_BYTE, icosahedronFaces);
+    glDrawElements(GL_TRIANGLES, 45, GL_UNSIGNED_BYTE, arrowFaces);
     
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
 	
     static NSTimeInterval lastDrawTime;
     if (lastDrawTime)
     {
         NSTimeInterval timeSinceLastDraw = [NSDate timeIntervalSinceReferenceDate] - lastDrawTime;
-        rot+=20 * timeSinceLastDraw;                
+        rot+=50 * timeSinceLastDraw;                
     }
     lastDrawTime = [NSDate timeIntervalSinceReferenceDate];
+	
+	//int nVertices = sizeof(vertices) / sizeof(Vertex3D);
+	//int nFaces = sizeof(arrowFaces) / (sizeof(GLubyte) * 3);
+	//normals = [self vertexNormals:vertices nVertices:nVertices faces:arrowFaces nFaces:nFaces];
 	
 	NSLog(@"View drawn");
     
 }
 -(void)setupView:(GLView*)view
 {
-	NSLog(@"Setup view");
+	
 	const GLfloat zNear = 0.01, zFar = 1000.0, fieldOfView = 45.0; 
 	GLfloat size; 
 	glEnable(GL_DEPTH_TEST);
@@ -354,9 +431,52 @@
 	glViewport(0, 0, rect.size.width, rect.size.height);  
 	glMatrixMode(GL_MODELVIEW);
 	
-	glLoadIdentity(); 
+	// Enable normals
+	glEnableClientState(GL_NORMAL_ARRAY);
 	
-	NSLog(@"View setup");
+	// Create the materials
+	GLfloat ambientAndDiffuse[] = {0.0, 0.1, 0.9, 1.0};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, ambientAndDiffuse);
+    GLfloat specular[] = {0.3, 0.3, 0.3, 1.0};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 25.0);
+	
+	// Enable lighting
+    glEnable(GL_LIGHTING);
+	
+    // Turn the first light on
+    glEnable(GL_LIGHT0);
+    
+    // Define the ambient component of the first light
+    const GLfloat light0Ambient[] = {0.1, 0.1, 0.1, 1.0};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0Ambient);
+    
+    // Define the diffuse component of the first light
+    const GLfloat light0Diffuse[] = {0.7, 0.7, 0.7, 1.0};
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0Diffuse);
+    
+    // Define the specular component and shininess of the first light
+    const GLfloat light0Specular[] = {0.7, 0.7, 0.7, 1.0};
+    const GLfloat light0Shininess = 0.4;
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0Specular);
+    
+    
+    // Define the position of the first light
+    const GLfloat light0Position[] = {0.0, 5.0, 5.0, 0.0}; 
+    glLightfv(GL_LIGHT0, GL_POSITION, light0Position); 
+    
+    // Define a direction vector for the light, this one points right down the Z axis
+    const GLfloat light0Direction[] = {0.0, 0.0, -1.0};
+    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light0Direction);
+	
+    // Define a cutoff angle. This defines a 90° field of vision, since the cutoff
+    // is number of degrees to each side of an imaginary line drawn from the light's
+    // position along the vector supplied in GL_SPOT_DIRECTION above
+    glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 45.0);
+	
+	glLoadIdentity(); 
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	
 }
 
 
